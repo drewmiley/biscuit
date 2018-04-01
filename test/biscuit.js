3f951eeb61c2e24dfae976449da6970d9c646699
Drew Miley <drewmiley92@gmail.com>
2018-04-01 23:58:36 +0100

    Cleans up blah

--- test/biscuit.js
+++ test/biscuit.js
@@ -2,4 +2,4 @@ console.log('Testing Biscuits');
 
 console.log('Array Method - blah');
 
-console.log(`[1, 2].blah(a => a === 1) = ${ [1, 2].blah(a => a === 1) }`);
+console.log(`[1, 2].blah(a => a === 1, true) = ${ [1, 2].blah(a => a === 1, true) }`);
