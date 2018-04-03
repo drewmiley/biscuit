647c86951a462487e82f4560dc21b3f7b576e6ad
Drew Miley <drew.miley@opencastsoftware.com>
2018-04-03 08:35:12 +0100

    Adds some tests for count

--- test/biscuit.js
+++ test/biscuit.js
@@ -4,6 +4,9 @@ console.log('Array Method - blah');
 
 console.log(`[1, 2].blah(a => a === 1, true) = ${ [1, 2].blah(a => a === 1, true) }`);
 
-console.log('Array Method - count');
-
-console.log(`[1, 2].count(a => a === 1) = ${ [1, 2].count(a => a === 1) }`);
+console.log(`Array Method - count
+	[1, 2].count(a => a === 1) = ${ [1, 2].count(a => a === 1) }
+	[1, 2].count(a => a === 0) = ${ [1, 2].count(a => a === 0) }
+	[].count(a => a === 1) = ${ [].count(a => a === 1) }
+	[1, 2, 3, 4].count(a => a % 2) = ${ [1, 2, 3, 4].count(a => a % 2) }
+	[true, true, false].count(Boolean) = ${ [true, true, false].count(Boolean) }`);
