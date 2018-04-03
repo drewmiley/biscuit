a8147feaae7e1b09c9ca3aa348f05078662b6866
Drew Miley <drew.miley@opencastsoftware.com>
2018-04-03 22:02:06 +0100

    Adds even

--- test/biscuit.js
+++ test/biscuit.js
@@ -11,8 +11,14 @@ console.log(`Array Method - count
 	[1, 2, 3, 4].count(a => a % 2) = ${ [1, 2, 3, 4].count(a => a % 2) }
 	[true, true, false].count(Boolean) = ${ [true, true, false].count(Boolean) }`);
 
-	console.log(`Array Method - odd
-		[1, 2].odd() = [${ [1, 2].odd() }]
-		[].odd() = [${ [].odd() }]
-		[1, 2, 3, 4].odd() = [${ [1, 2, 3, 4].odd() }]
-		[true].odd() = [${ [true].odd() }]`);
+console.log(`Array Method - even
+	[1, 2].even() = [${ [1, 2].even() }]
+	[].even() = [${ [].even() }]
+	[1, 2, 3, 4].even() = [${ [1, 2, 3, 4].even() }]
+	[true].even() = [${ [true].even() }]`);
+
+console.log(`Array Method - odd
+	[1, 2].odd() = [${ [1, 2].odd() }]
+	[].odd() = [${ [].odd() }]
+	[1, 2, 3, 4].odd() = [${ [1, 2, 3, 4].odd() }]
+	[true].odd() = [${ [true].odd() }]`);
